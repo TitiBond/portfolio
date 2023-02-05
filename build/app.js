fbfdd60b943e85061de08c65e9cfae313f73ed0b
Titi <couanonthibault@hotmail.fr>
2023-02-05 18:45:11 +0100

    first commit for personal portfolio

---
+++ build/app.js
@@ -0,0 +1,6 @@
+// shiny effect on sub-title web dev fullstack
+let spans_dev = document.querySelectorAll('.span_dev');
+for (let i = 0; i < spans_dev.length; i++) {
+    spans_dev[i].style.animation = "brillance 5s "+ i*0.05+"s linear infinite";
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './build/*.{html,php,js}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'github' : "url('/build/img/GitHub_Logo.png')"
+      }
+    },
+    letterSpacing: {
+      '1': '0em',
+      '2': '0.025em',
+      '3': '0.05em',
+      '4': '0.1em',
+      '5': '0.15em',
+      '6': '0.2em',
+      '7': '0.25em',
+    },
+    colors: {
+      'white':{
+        DEFAULT : '#fff',
+      },
+      'main-color': {
+        DEFAULT : '#333',
+      },
+      'main-pink' : {
+        DEFAULT : '#FF00D1',
+      },
+      'main-green' : {
+        DEFAULT : '#54ECC4',
+      },
+      'grey-color' : {
+        DEFAULT : '#9B9B9B',
+      },
+    },
+  },
+  plugins: [],
+  
+}
