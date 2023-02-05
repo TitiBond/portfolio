e968cbae835e6cb12d5369ddc30a7b91e50ebab7
Thibault Couanon <couanonthibault@hotmail.fr>
2023-02-05 23:03:04 +0100

    update contact section

--- build/app.js
+++ build/app.js
@@ -4,3 +4,12 @@ for (let i = 0; i < spans_dev.length; i++) {
     spans_dev[i].style.animation = "brillance 5s "+ i*0.05+"s linear infinite";
 }
 
+
+/*DEBUT CONTACT FORM*/
+let contact_me_btn = document.querySelector('#contact_me_btn');
+let flip_card_inner = document.querySelector('#flip_card_inner');
+contact_me_btn.addEventListener('click',formRotation);
+function formRotation(){
+    flip_card_inner.style.transform = "rotateY(180deg)";
+}
+/*FIN CONTACT FORM*/
