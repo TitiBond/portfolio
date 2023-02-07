8d19112a893d162e139c3db2597d4368c1a94ee6
Titi <couanonthibault@hotmail.fr>
2023-02-07 18:01:28 +0100

    mobile slider with indicator

--- build/app.js
+++ build/app.js
@@ -5,16 +5,17 @@ for (let i = 0; i < spans_dev.length; i++) {
 }
 
 
-/*DEBUT CONTACT FORM*/
+/*START CONTACT FORM*/
 const contact_me_btn = document.querySelector('#contact_me_btn');
 const flip_card_inner = document.querySelector('#flip_card_inner');
 contact_me_btn.addEventListener('click',formRotation);
 function formRotation(){
     flip_card_inner.style.transform = "rotateY(180deg)";
 }
-/*FIN CONTACT FORM*/
+/*END CONTACT FORM*/
 
-/*DEBUT MENU BURGER*/
+
+/*START BURGER MENU*/
 const hamburger_btn = document.querySelector('#hamburger_button');
 const mobile_menu = document.querySelector('#mobile_menu');
 const titi_home_icon = document.querySelector('#titi_home_icon');
@@ -27,5 +28,41 @@ function toggleMenu(){
 
 hamburger_btn.addEventListener('click',toggleMenu);
 mobile_menu.addEventListener('click',toggleMenu);
+/*END BURGER MENU*/
 
-/*FIN MENU BURGER*/
+
+
+/*START SLIDER*/
+let carousel = document.querySelector('#slider_container');
+let indicators = document.querySelectorAll('.indicator');
+let elements = document.querySelectorAll('#slider_container > *');
+console.log(elements);
+let currentIndex = 0;
+
+function renderIndicator() {
+    for (var i = 0; i < indicators.length; i++) {
+        if (currentIndex == i){
+            indicators[i].checked = true;
+        }else{
+            indicators[i].checked = false;
+        }
+    }
+}    
+let observer = new IntersectionObserver(function(entries, observer) {
+  // find the entry with the largest intersection ratio
+  var activated = entries.reduce(function (max, entry) {
+    return (entry.intersectionRatio > max.intersectionRatio) ? entry : max;
+  });
+  if (activated.intersectionRatio > 0) {
+    currentIndex = elementIndices[activated.target.getAttribute("id")];
+    renderIndicator();
+  }
+}, {
+  root:carousel, threshold:0.5
+});
+var elementIndices = {};
+for (var i = 0; i < elements.length; i++) {
+  elementIndices[elements[i].getAttribute("id")] = i;
+  observer.observe(elements[i]);
+}
+/*END SLIDER*/
